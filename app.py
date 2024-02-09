55accf85eb4b7373d18cb1f698dac74130ecb818
sloggo <70816908+sloggo@users.noreply.github.com>
2024-02-09 18:08:11 +0000

    Create club management page for accepting requests

--- app.py
+++ app.py
@@ -53,6 +53,48 @@ def admin():
 
     return render_template('/admin.html', current_user=current_user, role=role_data[5], users_data=user_data)
 
+@app.route("/myclub")
+def myclub():
+    # fetch role of current user
+    conn = db.connect('db/user_data.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT Role FROM Users WHERE UserID = ?', (current_user.id,))
+    role_data = cursor.fetchone()
+
+    cursor.execute('SELECT * FROM Clubs WHERE CoordinatorID = ?', (current_user.id,))
+    club_owner = cursor.fetchone()
+
+    if role_data[0] == 'Coordinator' and club_owner:
+        cursor.execute('''
+                    SELECT u.UserID, u.Username, u.Contact, u.Email, c.RequestStatus
+                    FROM Users u 
+                    JOIN ClubMemberships c ON u.UserID = c.UserID 
+                    WHERE c.ClubID = ?
+                ''', (club_owner[0],))
+        club_users = cursor.fetchall()
+        return render_template('/myclubmanage.html', current_user=current_user, club_data=club_owner, club_users=club_users)
+    elif role_data[0] == 'Coordinator' and club_owner is None:
+        return render_template('/myclubcreate.html', current_user=current_user)
+
+@app.route("/updateClubMember", methods=['GET', 'POST'])
+def updateClubMember():
+    conn = db.connect('db/user_data.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Users WHERE UserID = ?', (current_user.id,))
+    user_data = cursor.fetchone()
+    if user_data[5] == "Coordinator":
+
+        approved = ''
+        if request.form['approval'] is not None and request.form['approval'] == 'on':
+            approved = 'Approved'
+            cursor.execute('UPDATE ClubMemberships SET RequestStatus = ? WHERE UserID = ?',
+                           (approved, int(request.form['userID'])))
+            conn.commit()
+
+        return redirect(url_for('myclub'))
+    else:
+        return redirect(url_for('home'))
+
 @app.route('/explore')
 def explore():
     conn = db.connect('db/user_data.db')
