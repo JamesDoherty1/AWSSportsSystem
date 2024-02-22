77eec0f5ecf8a26bea8625f8f14a4f7869ec4103
sloggo <70816908+sloggo@users.noreply.github.com>
2024-02-22 21:03:29 +0000

    Add event manage table on myclub dashboard

--- permissionpages.py
+++ permissionpages.py
@@ -25,8 +25,25 @@ def myclub():
                         WHERE c.ClubID = ?
                     ''', (club_owner[0],))
             club_users = cursor.fetchall()
-            print(club_users)
-            return render_template('/myclubmanage.html', current_user=current_user, club_data=club_owner, club_users=club_users)
+
+            cursor.execute('''
+                SELECT * from Events WHERE ClubID = ?
+            ''', (club_owner[0],))
+            event_data = cursor.fetchall()
+            registered_data = []
+
+            for event in event_data:
+                cursor.execute('''
+                                SELECT Users.UserID, Users.Username, Users.Contact, Users.Email, EventRegistrations.Status
+                                FROM Users
+                                JOIN EventRegistrations ON Users.UserID = EventRegistrations.UserID
+                                WHERE EventRegistrations.EventID = ?
+                            ''', (event[0],))
+                registered_data.append(cursor.fetchall())
+
+            print(registered_data)
+
+            return render_template('/myclubmanage.html', current_user=current_user, club_data=club_owner, club_users=club_users, event_data=event_data, registered_data=registered_data)
         elif role_data[0] == 'Coordinator' and club_owner is None:
             return render_template('/myclubcreate.html', current_user=current_user)
 
